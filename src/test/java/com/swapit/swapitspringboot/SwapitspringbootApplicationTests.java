fae0c8a069da5aee9495762b2c71c616666b54a8
islem <elhamdiislem18@gmail.com>
2025-04-27 14:11:56 +0100

    Initial commit

---
+++ src/test/java/com/swapit/swapitspringboot/SwapitspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapit.swapitspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapitspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
